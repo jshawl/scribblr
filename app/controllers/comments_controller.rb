fdf3e7eb73f0710c59215da4db9bc5b5948d993c
Samuel Key <s.key08@gmail.com>
2015-07-29 19:17:01 -0400

    trying to get comments in posts show view

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,50 @@
+class CommentsController < ApplicationController
+  # index
+  def index
+    @comments = Comment.all
+  end
+
+  # new
+  def new
+    @post = Post.find(params[:post_id])
+    @post = Comment.new
+  end
+
+  # create
+  def create
+    @post = Post.find(params[:post_id])
+    @comments = @post.comments.create(comment_params)
+    redirect_to post_path(@post)
+  end
+
+  # show
+  def show
+    @comment = Comment.find(params[:id])
+  end
+
+  # edit
+  def edit
+    @comment = Comment.find(params[:id])
+    @post = Post.find(params[:post_id])
+  end
+
+  # update
+  def update
+    @comment = Comment.find(params[:id])
+    @comment.update(comment_params)
+    redirect_to post_comment_path
+  end
+
+  # destroy
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    @post = Post.find(params[:post_id])
+    redirect_to post_path(@post)
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:author, :content)
+  end
+end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,1 +1,54 @@
-class PostsController
+class PostsController < ApplicationController
+
+  #index
+  def index
+    @posts = Post.all.order(:id).reverse
+  end
+
+  #new
+  def new
+    @post = Post.new
+  end
+
+  #create
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      redirect_to new_post_path
+    end
+  end
+
+  #show
+  def show
+    @post = Post.find(params[:id])
+    @comment = Comment.new
+    @comments = Comment.all  #trying to get comments to show in the post show view
+  end
+
+  # edit
+def edit
+  @post = Post.find(params[:id])
+end
+
+  #update
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to post_path
+  end
+
+  #destroy
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :author, :content)
+  end
+
+end
