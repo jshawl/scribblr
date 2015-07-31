9f8ea0bccb158dd371a9f1186ad88f161fdcdc5f
Samuel Key <s.key08@gmail.com>
2015-07-30 20:42:47 -0400

    added a home page link and some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,11 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+Comment.destroy_all
+Post.destroy_all
 
-post1 = Post.create(title: "Post1", author: "Sam", content: "Post1 Post1 Post1 Post1 ")
-post2 = Post.create(title: "Post2", author: "Sam", content: "Post2 Post2 Post2 Post2 ")
+
+post1 = Post.create(title: "Post 1", author: "Sam", content: "Post1 Post1 Post1 Post1 ")
+post2 = Post.create(title: "Post 2", author: "Sam", content: "Post2 Post2 Post2 Post2 ")
+
+Comment.create(author: "Sam", content:"First!!!!", post_id:post1.id)
